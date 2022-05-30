34e1526c54a5173ef7f1014674e0a8ab1b13bac1
thuthi <whquddn55@gmail.com>
2022-05-30 21:25:01 +0900

    [Add]: setStatsTest for Liquid.java

---
+++ tests/src/test/java/LiquidTest.java
@@ -0,0 +1,46 @@
+import mindustry.content.*;
+import mindustry.type.*;
+import mindustry.world.meta.*;
+import org.junit.jupiter.api.*;
+
+class LiquidTest{
+
+    @BeforeAll
+    static void beforeAll() {
+        ApplicationTests.launchApplication();
+    }
+
+    /**
+     * Purpose: set Stats to rendering table ui
+     * Input: none
+     * Expected: return SUCCESS
+     * oil.stats.toMap().get(Stat.explosiveness.category) -> NotNull
+     * oil.stats.toMap().get(Stat.flammability.category) -> NotNull
+     * oil.stats.toMap().get(Stat.temperature.category) -> NotNull
+     * oil.stats.toMap().get(Stat.heatCapacity.category) -> NotNull
+     * oil.stats.toMap().get(Stat.viscosity.category) -> NotNull
+     *
+     * oil.stats.toMap().get(Stat.explosiveness.category).getNull(Stat.explosiveness) -> NotNull
+     * oil.stats.toMap().get(Stat.flammability.category).getNull(Stat.flammability) -> NotNull
+     * oil.stats.toMap().get(Stat.temperature.category).getNull(Stat.temperature) -> NotNull
+     * oil.stats.toMap().get(Stat.heatCapacity.category).getNull(Stat.heatCapacity) -> NotNull
+     * oil.stats.toMap().get(Stat.viscosity.category).getNull(Stat.viscosity) -> NotNull
+     */
+    @Test
+    void setStatsTest() {
+        Liquid oil = Liquids.oil;
+        oil.setStats();
+
+        Assertions.assertNotNull(oil.stats.toMap().get(Stat.explosiveness.category));
+        Assertions.assertNotNull(oil.stats.toMap().get(Stat.flammability.category));
+        Assertions.assertNotNull(oil.stats.toMap().get(Stat.temperature.category));
+        Assertions.assertNotNull(oil.stats.toMap().get(Stat.heatCapacity.category));
+        Assertions.assertNotNull(oil.stats.toMap().get(Stat.viscosity.category));
+
+        Assertions.assertNotNull(oil.stats.toMap().get(Stat.explosiveness.category).getNull(Stat.explosiveness));
+        Assertions.assertNotNull(oil.stats.toMap().get(Stat.flammability.category).getNull(Stat.flammability));
+        Assertions.assertNotNull(oil.stats.toMap().get(Stat.temperature.category).getNull(Stat.temperature));
+        Assertions.assertNotNull(oil.stats.toMap().get(Stat.heatCapacity.category).getNull(Stat.heatCapacity));
+        Assertions.assertNotNull(oil.stats.toMap().get(Stat.viscosity.category).getNull(Stat.viscosity));
+    }
+}
