a103c258ebb12d60fdb73df1c412046db2d89bc0
thuthi <whquddn55@gmail.com>
2022-06-06 02:19:58 +0900

    [Add]: noAbilitySetStateTest for UnitType.java

--- tests/src/test/java/UnitTypeTest.java
+++ tests/src/test/java/UnitTypeTest.java
@@ -1,5 +1,11 @@
+import arc.func.*;
+import mindustry.content.*;
 import mindustry.entities.abilities.*;
+import mindustry.entities.bullet.*;
+import mindustry.gen.*;
+import mindustry.graphics.*;
 import mindustry.type.*;
+import mindustry.type.ammo.*;
 import org.junit.jupiter.api.*;
 
 class UnitTypeTest {
@@ -15,7 +21,7 @@ class UnitTypeTest {
      */
     @Test
     void noLocalizedNameNullTest() {
-        UnitType testUnit = new UnitType("testUnit"){{
+        UnitType testUnit = new UnitType("testUnit1"){{
             abilities.add(new EnergyFieldAbility(35f, 65f, 180f){{
                 statusDuration = 60f * 6f;
                 maxTargets = 25;
@@ -23,6 +29,29 @@ class UnitTypeTest {
         }};
         Assertions.assertThrows(NullPointerException.class, () -> {
             testUnit.setStats();
+        });
+    }
+
+    /**
+     * Purpose: setStats to render ui
+     * Input:
+     * Expected: No Throws NullPointerException
+     */
+    @Test
+    void noAbilitySetStateTest() {
+        UnitType[] nonAbilityUnits = {
+        UnitTypes.mace, UnitTypes.dagger, UnitTypes.crawler, UnitTypes.fortress, UnitTypes.scepter, UnitTypes.reign, UnitTypes.vela,
+        UnitTypes.corvus, UnitTypes.atrax,
+        UnitTypes.spiroct, UnitTypes.arkyid, UnitTypes.toxopid,
+        UnitTypes.flare, UnitTypes.eclipse, UnitTypes.horizon, UnitTypes.zenith, UnitTypes.antumbra,
+        UnitTypes.mono, UnitTypes.mega, UnitTypes.quad, UnitTypes.alpha, UnitTypes.beta, UnitTypes.gamma,
+        UnitTypes.risso, UnitTypes.minke, UnitTypes.sei, UnitTypes.retusa, UnitTypes.cyerce,
+        UnitTypes.navanax, UnitTypes.block
+        };
+        Assertions.assertDoesNotThrow(() -> {
+            for(UnitType unitType : nonAbilityUnits){
+                unitType.setStats();
+            }
         });
     }
 }
