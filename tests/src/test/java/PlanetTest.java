a364b822c3009245eca74bf5733d9f6afb3d9d8f
thuthi <whquddn55@gmail.com>
2022-06-06 00:36:52 +0900

    [Add]: getRotationTests for PlanetTest.java

--- tests/src/test/java/PlanetTest.java
+++ tests/src/test/java/PlanetTest.java
@@ -4,9 +4,14 @@ import mindustry.world.meta.*;
 import org.junit.jupiter.api.*;
 
 class PlanetTest{
+    static private Planet planet1;
+    static private Planet planet2;
+
     @BeforeAll
     static void beforeAll() {
         ApplicationTests.launchApplication();
+        planet1 = new Planet("planet1", null, 10f, 5);
+        planet2 = new Planet("planet2", planet1, 10f);
     }
 
     /**
@@ -17,8 +22,7 @@ class PlanetTest{
      */
     @Test
     void getLastSectorEmptyTest() {
-        Planet planet = new Planet("planet1", null, 10f, 5);
-        Assertions.assertEquals(planet.getLastSector().id, 0);
+        Assertions.assertEquals(planet1.getLastSector().id, 0);
     }
 
     /**
@@ -29,7 +33,30 @@ class PlanetTest{
      */
     @Test
     void getLastSectorNullTest() {
-        Planet planet = new Planet("planet2", null, 10f);
-        Assertions.assertNull(planet.getLastSector());
+        Assertions.assertNull(planet2.getLastSector());
+    }
+
+    /**
+     * Purpose: calculate current rotation value when tidalLocked
+     * Input: none
+     * Expected: return SUCCESS
+     * planet2.getRotation() == -26.08532f
+     */
+    @Test
+    void getRotationLockTest() {
+        planet2.tidalLock = true;
+        Assertions.assertEquals(planet2.getRotation(), -26.08532f);
+    }
+
+    /**
+     * Purpose: calculate current rotation value when tidalUnLocked
+     * Input: none
+     * Expected: return SUCCESS
+     * planet2.getRotation() == 61.520863f
+     */
+    @Test
+    void getRotationUnLockTest() {
+        planet2.tidalLock = false;
+        Assertions.assertEquals(planet2.getRotation(), 61.520863f);
     }
 }
